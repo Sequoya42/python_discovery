372569ec260dfe6360a6a6dce028e5f8ffeb4481
Raphael BAUM <rbaum@e1r4p17.42.fr>
2016-10-21 18:00:41 +0200

    euler

--- euler/euler.py
+++ euler/euler.py
@@ -1,10 +1,10 @@
 a, b = 3, 5
-s = 0
+res = 0
 
 for i in xrange(1, 1000):
 	if (a * i) < 1000: 
-		s += (a *i)
+		res += (a *i)
 	if (b * i) < 1000 and (b * i) % a != 0:
-		s += (b * i)
+		res += (b * i)
 
-print s
+print res
